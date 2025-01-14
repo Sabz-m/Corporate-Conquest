1ac77c576797f1b9d29e0d6abb0dbd94f5171e51
Sabz-m <Sabz-m@live.com>
2025-01-14 16:35:39 +0000

    testing redux toolkit

--- src/Reducers/Index.jsx
+++ src/Reducers/Index.jsx
@@ -1,3 +1,7 @@
 /*Role: Combines all reducers into a single root reducer.
 Purpose: This file combines the individual reducers (gameReducer, playerReducer, etc.) into one root reducer using combineReducers().
 Example: It combines all the reducers and exports the root reducer for use in the Redux store.*/
+exports.CombatReducer = require("./CombatReducer");
+exports.GameReducer = require("./GameReducer");
+exports.InventoryReducer = require("./InventoryReducer");
+exports.PlayerReducer = require("./PlayerReducer");
